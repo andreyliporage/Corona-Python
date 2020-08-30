b81602f8e2164befcd2b704f895f062e5a5478fb
andreyliporage <andreyliporage@gmail.com>
2020-08-30 12:31:17 -0300

    refatorando funcoes de keyup e keydown

--- game_functions.py
+++ game_functions.py
@@ -7,16 +7,22 @@ def check_events(doctor):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                doctor.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                doctor.moving_left = True
+            check_keydown_events(event, doctor)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                doctor.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                doctor.moving_left = False
+            check_keyup_events(event, doctor)
+
+def check_keyup_events(event, doctor):
+    if event.key == pygame.K_RIGHT:
+        doctor.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        doctor.moving_left = False
+
+def check_keydown_events(event, doctor):
+    if event.key == pygame.K_RIGHT:
+        doctor.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        doctor.moving_left = True
 
 def update_screen(ai_setting, screen, doctor):
     screen.fill(ai_setting.bg_color)
